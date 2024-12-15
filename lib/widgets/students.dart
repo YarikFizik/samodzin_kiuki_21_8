7518eabdc19dcef4c041a76b42bcaa64af695089
YarikFizik <yaroslav.samodzin@nure.ua>
2024-12-15 20:39:46 +0200

    secondcommit
    lb2

--- lib/widgets/students.dart
+++ lib/widgets/students.dart
@@ -1,59 +1,86 @@
 import 'package:flutter/material.dart';
 import '../models/student.dart';
 import 'student_item.dart';
+import 'new_student.dart';
 
-class StudentsScreen extends StatelessWidget {
-  StudentsScreen({Key? key}) : super(key: key);
+class StudentsScreen extends StatefulWidget {
+  @override
+  _StudentsScreenState createState() => _StudentsScreenState();
+}
 
-  final List<Student> students = [
-    Student(
-      firstName: 'Іван',
-      lastName: 'Петров',
-      department: Department.it,
-      grade: 95,
-      gender: Gender.male,
-    ),
-    Student(
-      firstName: 'Марія',
-      lastName: 'Іваненко',
-      department: Department.finance,
-      grade: 88,
-      gender: Gender.female,
-    ),
-    Student(
-      firstName: 'Олександр',
-      lastName: 'Коваль',
-      department: Department.law,
-      grade: 76,
-      gender: Gender.male,
-    ),
-    Student(
-      firstName: 'Наталія',
-      lastName: 'Шевченко',
-      department: Department.medical,
-      grade: 92,
-      gender: Gender.female,
-    ),
-    Student(
-      firstName: 'Андрій',
-      lastName: 'Сидоренко',
-      department: Department.it,
-      grade: 60,
-      gender: Gender.male,
-    ),
+class _StudentsScreenState extends State<StudentsScreen> {
+  List<Student> _students = [
+    Student(firstName: 'Іван', lastName: 'Петров', department: Department.it, grade: 95, gender: Gender.male),
+    Student(firstName: 'Марія', lastName: 'Іваненко', department: Department.finance, grade: 88, gender: Gender.female),
   ];
+
+  void _addOrEditStudent([Student? student]) {
+    showModalBottomSheet(
+      context: context,
+      isScrollControlled: true,
+      builder: (_) {
+        return NewStudent(
+          student: student,
+          onSave: (newStudent) {
+            setState(() {
+              if (student != null) {
+                _students[_students.indexOf(student)] = newStudent;
+              } else {
+                _students.add(newStudent);
+              }
+            });
+          },
+        );
+      },
+    );
+  }
+
+  void _deleteStudent(int index) {
+    final removedStudent = _students[index];
+    setState(() {
+      _students.removeAt(index);
+    });
+
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        content: Text('${removedStudent.firstName} removed'),
+        action: SnackBarAction(
+          label: 'Undo',
+          onPressed: () {
+            setState(() {
+              _students.insert(index, removedStudent);
+            });
+          },
+        ),
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Студенти'),
+        title: const Text('Students'),
       ),
       body: ListView.builder(
-        itemCount: students.length,
+        itemCount: _students.length,
         itemBuilder: (context, index) {
-          return StudentItem(student: students[index]);
+          return Dismissible(
+            key: ValueKey(_students[index]),
+            onDismissed: (_) => _deleteStudent(index),
+            background: Container(color: Colors.red),
+            child: InkWell(
+              onTap: () => _addOrEditStudent(_students[index]),
+              child: StudentItem(student: _students[index]),
+            ),
+          );
         },
+      ),
+      floatingActionButton: FloatingActionButton.extended(
+        onPressed: () => _addOrEditStudent(),
+        label: const Text('Add Student'),
+        icon: const Icon(Icons.add),
+        backgroundColor: Colors.orange, // Изменённый цвет кнопки
       ),
     );
   }
